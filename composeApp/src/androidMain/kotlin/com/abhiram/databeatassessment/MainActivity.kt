92ead163c92d442c46dc836a3ea35d1973581bb1
Abhiram <abhiram090601@gmail.com>
2025-04-01 18:40:17 +0530

    chore: enable edge to edge

--- composeApp/src/androidMain/kotlin/com/abhiram/databeatassessment/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/abhiram/databeatassessment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.abhiram.databeatassessment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.abhiram.databeatassessment.core.presentation.data_beat_assessment_app.App
@@ -10,7 +11,7 @@ import com.abhiram.databeatassessment.core.presentation.data_beat_assessment_app
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             App()
         }
