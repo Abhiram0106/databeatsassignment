d9858dc0c0f18a4d868ba6a87dee63ec98d10a22
Abhiram <abhiram090601@gmail.com>
2025-04-01 16:39:04 +0530

    chore: setup app global state

---
+++ composeApp/src/commonMain/kotlin/com/abhiram/databeatassessment/core/navigation/TopLevelDestinations.kt
@@ -0,0 +1,22 @@
+package com.abhiram.databeatassessment.core.navigation
+
+import databeatassessment.composeapp.generated.resources.Res
+import databeatassessment.composeapp.generated.resources.home
+import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.StringResource
+import kotlin.reflect.KClass
+
+enum class TopLevelDestination(
+    val icon: DrawableResource,
+    val displayText: StringResource,
+    val route: @Serializable KClass<*>,
+    val baseRoute: @Serializable KClass<*>
+) {
+    Home(
+        icon = Res.drawable.ic_rounded_home,
+        displayText = Res.string.home,
+        route = Screens.Home::class,
+        baseRoute = Graphs.Home::class
+    ),
+}
---
+++ composeApp/src/commonMain/kotlin/com/abhiram/databeatassessment/core/presentation/data_beat_assessment_app/AppState.kt
@@ -0,0 +1,104 @@
+package com.abhiram.databeatassessment.core.presentation.data_beat_assessment_app
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.abhiram.databeatassessment.core.navigation.Graphs
+import com.abhiram.databeatassessment.core.navigation.TopLevelDestination
+import kotlinx.coroutines.CoroutineScope
+import kotlin.reflect.KClass
+
+@Composable
+fun rememberFishoAppState(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavHostController = rememberNavController(),
+): FishoAppState {
+
+    return remember(
+        coroutineScope,
+        navController
+    ) {
+        FishoAppState(
+            coroutineScope,
+            navController
+        )
+    }
+}
+
+class FishoAppState(
+    coroutineScope: CoroutineScope,
+    val navController: NavHostController,
+) {
+
+    private val previousDestination = mutableStateOf<NavDestination?>(null)
+    val currentDestination: NavDestination?
+        @Composable get() {
+            val currentEntry = navController.currentBackStackEntryFlow
+                .collectAsState(initial = null)
+
+            // Fallback to previousDestination if currentEntry is null
+            return currentEntry.value?.destination.also { destination ->
+                if (destination != null) {
+                    previousDestination.value = destination
+                }
+            } ?: previousDestination.value
+        }
+
+    val topLevelDestinations: Array<TopLevelDestination> =
+        TopLevelDestination.entries.toTypedArray()
+
+    @Composable
+    fun isBottomNavItemSelected(route: KClass<*>): Boolean {
+        return currentDestination.isRouteInHierarchy(route)
+    }
+
+    @Composable
+    fun showBottomBar(): Boolean {
+        return topLevelDestinations.firstOrNull { dest ->
+            currentDestination?.hasTopScreenAs(dest.route) == true
+        } != null
+    }
+
+    fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+        if (navController.currentDestination.hasTopScreenAs(topLevelDestination.route)) {
+            return
+        }
+
+        val topLevelNavOptions = navOptions {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            launchSingleTop = true
+//             Restore state when reselecting a previously selected item
+            restoreState = true
+        }
+
+        navController.navigate(
+            route = when (topLevelDestination) {
+                TopLevelDestination.Home -> {
+                    Graphs.Home
+                }
+            },
+            navOptions = topLevelNavOptions
+        )
+    }
+
+    private fun NavDestination?.hasTopScreenAs(route: KClass<*>): Boolean {
+        val topDestination = this?.hierarchy?.firstOrNull() ?: return false
+        return topDestination.hasRoute(route)
+    }
+
+    private fun NavDestination?.isRouteInHierarchy(route: KClass<*>) =
+        this?.hierarchy?.any { dest ->
+            dest.hasRoute(route)
+        } ?: false
+}
