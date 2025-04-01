55a866c7eae9b4fd3be385692d2008158fcd68f3
Abhiram <abhiram090601@gmail.com>
2025-04-01 16:19:43 +0530

    build: add compose navigation and kotlinx serialization dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,6 +43,9 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(libs.jetbrains.compose.navigation)
+            implementation(libs.kotlinx.serialization.json)
         }
     }
 }
